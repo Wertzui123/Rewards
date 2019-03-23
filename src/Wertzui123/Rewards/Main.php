93d9c021fe71212ebb1ed4f3543d86e9afa3a37f
Wertzui123 <46199283+Wertzui123@users.noreply.github.com>
2019-03-23 13:20:51 +0100

    Update Main.php

--- src/Wertzui123/Rewards/Main.php
+++ src/Wertzui123/Rewards/Main.php
@@ -6,30 +6,15 @@ namespace Wertzui123\Rewards;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\item\Item;
 use pocketmine\Player;
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 use pocketmine\Server;
-use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\event\player\PlayerQuitEvent;
-use pocketmine\event\block\BlockBreakEvent;
-use pocketmine\event\block\BlockPlaceEvent;
-use pocketmine\event\player\PlayerDropItemEvent;
-use pocketmine\event\player\PlayerExhaustEvent;
-use pocketmine\event\player\PlayerItemConsumeEvent;
-use pocketmine\event\player\PlayerInteractEvent;
-use pocketmine\event\player\PlayerItemHeldEvent;
-use pocketmine\event\entity\ProjectileHitEvent;
-use pocketmine\event\entity\ProjectileLaunchEvent;
-use pocketmine\network\mcpe\protocol\ChangeDimensionPacket;
 use pocketmine\event\Listeners;
 use pocketmine\utils\TextFormat;
-use pocketmine\scheduler\Task;
 use pocketmine\command\ConsoleCommandSender;
 use pocketmine\level\Level;
-use many1337\task\GuardianTask;
 use pocketmine\math\Vector3;
 
 class Main extends PluginBase implements Listener{
