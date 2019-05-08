b7c663e20e9b10077942a23102766ee437a3eb13
Wertzui123 <46199283+Wertzui123@users.noreply.github.com>
2019-05-08 20:32:26 +0200

    Update 2.0

--- src/Wertzui123/Rewards/Main.php
+++ src/Wertzui123/Rewards/Main.php
@@ -4,91 +4,78 @@ declare(strict_types=1);
 
 namespace Wertzui123\Rewards;
 
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
 use pocketmine\Player;
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
-use pocketmine\Server;
-use pocketmine\event\Listeners;
-use pocketmine\utils\TextFormat;
-use pocketmine\command\ConsoleCommandSender;
-use pocketmine\level\Level;
-use pocketmine\math\Vector3;
 use pocketmine\event\player\PlayerJoinEvent;
 
 class Main extends PluginBase implements Listener{
 
+    public $configversion;
+
 	public function onEnable() : void{ 
 	    $this->saveResource("config.yml");
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->configversion = 2.0;
+        $this->ConfigUpdater();
+        $this->getServer()->getCommandMap()->register("reward", new reward($this));
 	}
 	
 	
-		public function onJoin(PlayerJoinEvent $event){
-if(!file_exists($this->getDataFolder() . $event->getPlayer()->getName() . ".yml")){
-$cfg = new Config($this->getDataFolder() . $event->getPlayer()->getName() . ".yml", Config::YAML);
-$player = $event->getPlayer();
-$today = new \DateTime("now");
-$now = $today->format("d.m.Y H:i");
-$cfg->set("until", $now);
-$cfg->save();
-}
-	}
-	
-	
-	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
-		switch($command->getName()){
-			case "reward":
-			
-		$name = $sender->getName();
-		$cfg = new Config($this->getDataFolder() . $name . ".yml", Config::YAML);
-		$config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
-		$until = $cfg->get("until");
-		$today = new \DateTime("now");
-		$nopermission = $config->get("no_permission");
-		$usage = $config->get("usage");
-		$runingame = $config->get("run_ingame");
-		$alreadygotreward = $config->get("already_got_reward");
-		$alreadygotreward = str_replace("{until}", $until, $alreadygotreward);
-		$gotrewardsucces = $config->get("got_reward_succes");
-		$rewardcommand = $config->get("command_wich_will_be_executed");
-		$timeformat = $config->get("time_format");
-		$waittime = $config->get("wait_time");
-		$now = $today->format($timeformat);
-		$until2 = date ($timeformat, strtotime ($now ."+" . $waittime));
-
-		if(!$sender instanceof Player) {
-			$sender->sendMessage($runingame);
-			return true;
-		}
-		if($sender->hasPermission("rewards.claim")) {
-				if(!empty($args[0])){
-					$sender->sendMessage("$usage");
-				} else {
-		if($sender === $now) {
-		} else {
-        if($now >= $until){
-		$sender->sendMessage($gotrewardsucces);
-		$rewardcommand = str_replace("{player}", $name, $rewardcommand);
-		$this->getServer()->dispatchCommand(new ConsoleCommandSender(), $rewardcommand); 
-		$cfg->set("until", $until2);
-		$cfg->save();
-	 }else{
-		 $sender->sendMessage("$alreadygotreward");
-	 }
-      }
-      }
-      } else{
-			$sender->sendMessage($nopermission);
-     }
-     return true;
-		}
-  }
+		public function onJoin(PlayerJoinEvent $event)
+        {
+            if (!$this->isjoinedbefor($event->getPlayer())) {
+                $cfg = new Config($this->getDataFolder() . "players.yml", Config::YAML);
+                $player = $event->getPlayer();
+                $today = new \DateTime("now");
+                $f = $this->ConfigArray()["time_format"];
+                $now = $today->format($f);
+                $cfg->set($player->getName(), $now);
+                $cfg->save();
+            }
+        }
   
-	public function onDisable() : void{
-	}
+	public function isjoinedbefor(Player $player)
+    {
+        $jc = $this->getJoins();
+        if (!$jc->get($player->getName()) == true) {
+            $jc->set($player->getName(), true);
+            $jc->save();
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public function getJoins(){
+	    $f = new Config($this->getDataFolder()."joins.yml", Config::YAML);
+	    return $f;
+    }
+
+    public function ConfigUpdater()
+    {
+        $c = $this->ConfigArray();
+        $cv = $c["config_version"] ?? 0;
+        if (file_exists($this->getDataFolder() . "config.yml")) {
+            if ($cv != $this->configversion) {
+                $this->getLogger()->info("§cYour Config isn't the latest. §6We renamed your old config to §bconfig-" . $cv . ".yml §6and created a new config.yml. §aHave fun!");
+                rename($this->getDataFolder() . "config.yml", "config-" . $cv . ".yml");
+                $this->saveResource("config.yml");
+            }
+        } else {
+            $this->saveResource("config.yml");
+
+        }
+    }
+
+    public function ConfigArray()
+    {
+        $c = new Config($this->getDataFolder() . "config.yml");
+        $c = $c->getAll();
+        return $c;
+    }
+
 }
 // This Plugin was written by Wertzui123 and you're not allowed to copy or clone it into you're plugin!
 // You also musn't change the author or the license.
